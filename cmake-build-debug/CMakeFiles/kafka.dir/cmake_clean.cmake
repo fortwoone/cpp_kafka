513f3af5f0ec370af491feb6a47080d066904025
fortwoone <denauxarthur@gmail.com>
2025-09-15 18:14:08 +0200

    [any message]

---
+++ cmake-build-debug/CMakeFiles/kafka.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kafka.dir/src/main.cpp.o"
+  "CMakeFiles/kafka.dir/src/main.cpp.o.d"
+  "CMakeFiles/kafka.dir/src/requests.cpp.o"
+  "CMakeFiles/kafka.dir/src/requests.cpp.o.d"
+  "kafka"
+  "kafka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kafka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
