b1804dc6155f860a7aafe8522b1b29ad215c9a87
fortwoone <denauxarthur@gmail.com>
2025-09-23 10:52:50 +0200

    Part 16: fetch multiple messages from disk (debug commit)

--- src/cluster_metadata.cpp
+++ src/cluster_metadata.cpp
@@ -152,14 +152,14 @@ namespace cpp_kafka{
                             auto& part_payload = rec_value.payload.emplace<PartitionPayload>();
 
                             part_payload.partition_id = read_be_and_advance<fint>(buf, offset);
-                            offset++;  // Jump one byte ahead before reading to avoid reading incorrect values.
                             for (ubyte k = 0; k < 16; ++k){
-                                if (k == 5){
-                                    part_payload.topic_uuid[k] = part_payload.topic_uuid[k - 1];
-                                    continue;
-                                }
+//                                if (k == 5){
+//                                    part_payload.topic_uuid[k] = part_payload.topic_uuid[k - 1];
+//                                    continue;
+//                                }
                                 part_payload.topic_uuid[k] = read_and_advance<ubyte>(buf, offset);
                             }
+                            offset++;  // Jump one byte ahead before reading to avoid reading incorrect values.
 
                             // Encoded as a varint, so we need to deduce 1 from this value.
                             auto repl_arr_size = unsigned_varint_t::decode_and_advance(buf, offset) - 1;
