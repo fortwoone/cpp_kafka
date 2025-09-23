c27cc9b6cf12c778a5c7824cd5386f9a34f517b8
fortwoone <denauxarthur@gmail.com>
2025-09-23 09:27:25 +0200

    Part 16: fetch multiple messages from disk (debug commit)

--- src/requests.cpp
+++ src/requests.cpp
@@ -410,7 +410,6 @@ namespace cpp_kafka{
             for (ubyte k = 0; k < 16; ++k){
                 requested_uuids[i][k] = read_and_advance<ubyte>(buffer, offset);
             }
-            offset++;  // Jump one byte ahead to avoid reading incorrect values
             string uuid_as_str = {
                 reinterpret_cast<const char*>(requested_uuids[i].data()),
                 16
